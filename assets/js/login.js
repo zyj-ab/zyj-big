191678474dd0ed364a1ce44fc3ce49dd76c98f71
zyj <zyj@qq.com>
2020-10-17 19:13:10 +0800

    第一次上传

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#link_reg').on('click',function(){
+        $('.reg-box').show()
+        $('.login-box').hide()
+    })
+    $('#link_login').on('click',function(){
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+})
